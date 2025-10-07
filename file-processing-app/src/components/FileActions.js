09c9d2c6144b0348a1a0d32c196cee1bbe345e9d
Tejeswar03 <tejeswar@sqrx.com>
2025-10-07 10:43:15 +0530

    Update FileActions.js

--- file-processing-app/src/components/FileActions.js
+++ file-processing-app/src/components/FileActions.js
@@ -12,11 +12,11 @@ export default function FileActions({
 
   // Mock server URLs (for display purposes)
   const serverUrls = {
-    upload: 'wss://34.173.239.174:6789',
-    regular: 'https://34.173.239.174:5010/upload_regular',
-    encoded: 'https://34.173.239.174:5010/upload_encoded',
-    encrypted: 'https://34.173.239.174:5010/upload_encrypted',
-    chunked: 'https://34.173.239.174:5010/upload_chunked'
+    upload: 'wss://ws.files.attackvault.xyz',
+    regular: '  https://see.files.attackvault.xyz/upload_regular',
+    encoded: '  https://see.files.attackvault.xyz/upload_encoded',
+    encrypted: '  https://see.files.attackvault.xyz/upload_encrypted',
+    chunked: '  https://see.files.attackvault.xyz/upload_chunked'
   };
 
 
